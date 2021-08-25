ead566344951ba1ee84993b2dac4de9fa99009ba
Shrikant Sonawane_KH <83824454+ShrikantSonawane-KH@users.noreply.github.com>
2021-08-26 03:22:31 +0530

    Create Pro_descriptionDaoImpl.java

---
+++ src/main/java/property/dao/Pro_descriptionDaoImpl.java
@@ -0,0 +1,31 @@
+package property.dao;
+
+import java.util.List;
+
+import property.pojo.Pro_Description;
+
+
+
+public class Pro_descriptionDaoImpl implements Pro_descriptionDao{
+
+	public Pro_Description insertion(Pro_Description pro_description) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Pro_Description deletion(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Pro_Description updation(Pro_Description pro_description) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Pro_Description> showAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
