4c36cc426cd76e5640462bf980d4f0086e13a4c7
Shrikant Sonawane_KH <83824454+ShrikantSonawane-KH@users.noreply.github.com>
2021-08-26 03:21:45 +0530

    Create RenterDaoImpl.java

---
+++ src/main/java/property/dao/RenterDaoImpl.java
@@ -0,0 +1,30 @@
+package property.dao;
+
+import java.util.List;
+
+import property.pojo.Renter;
+
+
+public class RenterDaoImpl implements RenterDao{
+
+	public Renter insertion(Renter renter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Renter deletion(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Renter updation(Renter renter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Renter> showAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
