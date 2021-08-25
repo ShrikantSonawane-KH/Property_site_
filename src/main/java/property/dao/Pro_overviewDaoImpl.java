7283e05d7b3acef79f892db081d7fa3def637f8c
Shrikant Sonawane_KH <83824454+ShrikantSonawane-KH@users.noreply.github.com>
2021-08-26 03:21:27 +0530

    Create Pro_overviewDaoImpl.java

---
+++ src/main/java/property/dao/Pro_overviewDaoImpl.java
@@ -0,0 +1,30 @@
+package property.dao;
+
+import java.util.List;
+
+import property.pojo.Pro_overview;
+
+
+public class Pro_overviewDaoImpl implements Pro_overviewDao{
+
+	public Pro_overview insertion(Pro_overview pro_overview) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Pro_overview deletion(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Pro_overview updation(Pro_overview pro_overview) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Pro_overview> showAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
