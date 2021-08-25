d9b85a0fa2fd76d29ce55d7d90afaf637e2904dd
Shrikant Sonawane_KH <83824454+ShrikantSonawane-KH@users.noreply.github.com>
2021-08-26 03:21:37 +0530

    Create PropertyDaoImpl.java

---
+++ src/main/java/property/dao/PropertyDaoImpl.java
@@ -0,0 +1,30 @@
+package property.dao;
+
+import java.util.List;
+
+import property.pojo.Property;
+
+
+public class PropertyDaoImpl implements PropertyDao{
+
+	public Property insertion(Property property) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Property deletion(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Property updation(Property property) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Property> showAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
