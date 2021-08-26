8e500c87409475fb607f41f4e29f1de94af80936
Shrikant Sonawane_KH <83824454+ShrikantSonawane-KH@users.noreply.github.com>
2021-08-26 10:16:50 +0530

    Create Pro_addressDaoImpl.java

---
+++ src/main/java/property/dao/Pro_addressDaoImpl.java
@@ -0,0 +1,30 @@
+package property.dao;
+
+import java.util.List;
+
+import property.pojo.Pro_address;
+
+
+public class Pro_addressDaoImpl implements Pro_addressDao{
+
+	public Pro_address insertion(Pro_address pro_address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Pro_address deletion(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Pro_address updation(Pro_address pro_address) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<Pro_address> showAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
