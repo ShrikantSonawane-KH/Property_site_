7dbfe7f3f3889688dfbe96b6b7fd47cc8abe570a
Shrikant Sonawane_KH <83824454+ShrikantSonawane-KH@users.noreply.github.com>
2021-08-28 20:06:14 +0530

    Update Pro_image.java

--- src/main/java/property/pojo/Pro_image.java
+++ src/main/java/property/pojo/Pro_image.java
@@ -1,7 +1,6 @@
 package property.pojo;
 
 
-import java.util.Arrays;
 
 import javax.persistence.Embeddable;
 
@@ -10,12 +9,12 @@ import javax.persistence.Embeddable;
 public class Pro_image {
 
 	private String img_desc;
-	private byte[] image;				// think of import
+	private String image;
 	public Pro_image() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public Pro_image(String img_desc, byte[] image) {
+	public Pro_image(String img_desc, String image) {
 		super();
 		this.img_desc = img_desc;
 		this.image = image;
@@ -26,20 +25,17 @@ public class Pro_image {
 	public void setImg_desc(String img_desc) {
 		this.img_desc = img_desc;
 	}
-	public byte[] getImage() {
+	public String getImage() {
 		return image;
 	}
-	public void setImage(byte[] image) {
+	public void setImage(String image) {
 		this.image = image;
 	}
 	@Override
 	public String toString() {
-		return "Pro_image [img_desc=" + img_desc + ", image=" + Arrays.toString(image) + "]";
-	}
-	
-	
-	
-	
+		return "Pro_image [img_desc=" + img_desc + ", image=" + image + "]";
+	}				
+		
 	
 }
 
