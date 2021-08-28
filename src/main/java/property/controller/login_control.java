7ede20ca335a4ab6145f7093e04cb6a89cf12267
Shrikant Sonawane_KH <83824454+ShrikantSonawane-KH@users.noreply.github.com>
2021-08-29 04:51:44 +0530

    Update login_control.java

--- src/main/java/property/controller/login_control.java
+++ src/main/java/property/controller/login_control.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import property.dao.RenterDao;
+import property.dao.RenterDaoImpl;
 import property.service.LoginCheckDao;
 import property.service.LoginCheckDaoImpl;
 
@@ -22,7 +24,12 @@ public class login_control extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		
+		//////////
+		RenterDao renterdao = new RenterDaoImpl();
+		renterdao.showByCity("pune");
+		/////////
+		
 		LoginCheckDao logincheckdao = new LoginCheckDaoImpl();
 
 		String email = request.getParameter("email");
