dd60b91866c1625a68ace5e00b6e0d12a7b70e9a
Shrikant Sonawane_KH <83824454+ShrikantSonawane-KH@users.noreply.github.com>
2021-08-29 23:02:05 +0530

    Update Property.java

--- src/main/java/property/pojo/Property.java
+++ src/main/java/property/pojo/Property.java
@@ -22,12 +22,14 @@ public class Property {
 	private float deposite;
 	private String pro_type;
 	private String category;
+	
 	public Property() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public Property(String name, Pro_address address, Pro_Description description, String pro_for, Pro_image image,
-			float price, float deposite, String pro_type, String category) {
+		float price, float deposite, String pro_type, String category) {
 		super();
 		this.name = name;
 		this.address = address;
@@ -39,66 +41,86 @@ public class Property {
 		this.pro_type = pro_type;
 		this.category = category;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Pro_address getAddress() {
 		return address;
 	}
+
 	public void setAddress(Pro_address address) {
 		this.address = address;
 	}
+
 	public Pro_Description getDescription() {
 		return description;
 	}
+
 	public void setDescription(Pro_Description description) {
 		this.description = description;
 	}
+
 	public String getPro_for() {
 		return pro_for;
 	}
+
 	public void setPro_for(String pro_for) {
 		this.pro_for = pro_for;
 	}
+
 	public Pro_image getImage() {
 		return image;
 	}
+
 	public void setImage(Pro_image image) {
 		this.image = image;
 	}
+
 	public float getPrice() {
 		return price;
 	}
+
 	public void setPrice(float price) {
 		this.price = price;
 	}
+
 	public float getDeposite() {
 		return deposite;
 	}
+
 	public void setDeposite(float deposite) {
 		this.deposite = deposite;
 	}
+
 	public String getPro_type() {
 		return pro_type;
 	}
+
 	public void setPro_type(String pro_type) {
 		this.pro_type = pro_type;
 	}
+
 	public String getCategory() {
 		return category;
 	}
+
 	public void setCategory(String category) {
 		this.category = category;
 	}
+
 	@Override
 	public String toString() {
 		return "Property [name=" + name + ", address=" + address + ", description=" + description + ", pro_for="
 				+ pro_for + ", image=" + image + ", price=" + price + ", deposite=" + deposite + ", pro_type="
 				+ pro_type + ", category=" + category + "]";
 	}
+	
 	
 	
 	
