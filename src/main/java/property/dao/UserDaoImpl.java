4c7f72ebd434ad26e3ea1982478b63e921304d4a
Shrikant Sonawane_KH <83824454+ShrikantSonawane-KH@users.noreply.github.com>
2021-08-31 01:52:52 +0530

    Update UserDaoImpl.java

--- src/main/java/property/dao/UserDaoImpl.java
+++ src/main/java/property/dao/UserDaoImpl.java
@@ -2,9 +2,12 @@ package property.dao;
 
 import java.util.List;
 
+import javax.persistence.Query;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+import property.pojo.Renter;
 import property.pojo.User;
 import property.util.SessionUtil;
 
@@ -23,18 +26,50 @@ public class UserDaoImpl implements UserDao {
 	}
 
 	public User deletion(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Session session = SessionUtil.getFactory().openSession();
+		Transaction tx = session.beginTransaction();
+		
+		User user = session.get(User.class, id);
+		session.delete(user);
+	
+		tx.commit();
+		session.close();
+		return user;
+		
 	}
 
 	public User updation(User user) {
-		// TODO Auto-generated method stub
+
 		return null;
 	}
 
 	public List<User> showAll() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Session session = SessionUtil.getFactory().openSession();
+
+		String QueryString = "from User";
+		Query Query = session.createQuery(QueryString);
+		List<User> user = Query.getResultList();
+
+		session.close();
+		return user;
+
+	}
+
+	@Override
+	public User showById(int id) {
+		Session session = SessionUtil.getFactory().openSession();
+
+		Transaction tx = session.beginTransaction();
+
+		User user = session.get(User.class, id);
+
+		tx.commit();
+		session.close();
+
+		return user;
+		
 	}
 
 }
