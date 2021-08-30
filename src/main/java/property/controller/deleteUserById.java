10dd2ede2be6cc22dfb5a7448834449b64c685d1
Shrikant Sonawane_KH <83824454+ShrikantSonawane-KH@users.noreply.github.com>
2021-08-31 01:53:21 +0530

    Create deleteUserById.java

---
+++ src/main/java/property/controller/deleteUserById.java
@@ -0,0 +1,39 @@
+package property.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import property.dao.UserDao;
+import property.dao.UserDaoImpl;
+import property.pojo.User;
+
+
+public class deleteUserById extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+  
+    public deleteUserById() {
+        super();
+       
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		UserDao userdao = new UserDaoImpl();
+	
+			int id = Integer.parseInt(request.getParameter("id"));
+			User user = userdao.deletion(id);
+			
+			String UserName =user.getUsername();
+			String UserRole =user.getRole();
+			
+			response.sendRedirect("editRenter.jsp?message= Successfully removed user"+ " "+UserName +" "+"of role"+" "+UserRole+" "+"...!");
+	
+
+	}
+
+}
