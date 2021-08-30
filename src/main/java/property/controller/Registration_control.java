ed5aef6038f2d3589a9ae2118c4e8bd2af3ab6c9
Shrikant Sonawane_KH <83824454+ShrikantSonawane-KH@users.noreply.github.com>
2021-08-30 14:37:41 +0530

    Update Registration_control.java

--- src/main/java/property/controller/Registration_control.java
+++ src/main/java/property/controller/Registration_control.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServletResponse;
 import property.dao.UserDao;
 import property.dao.UserDaoImpl;
 import property.pojo.User;
+import property.service.RegistrationValidationDao;
+import property.service.RegistrationValidationDaoImpl;
 
 public class Registration_control extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -21,15 +23,24 @@ public class Registration_control extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		RegistrationValidationDao registrationvalidationdao = new RegistrationValidationDaoImpl();
+
 		UserDao userdao = new UserDaoImpl();
 
 		String email = request.getParameter("username");
 		String password = request.getParameter("password");
 		String role = request.getParameter("role");
 
-		User user = new User(email, password, role);
+		boolean validEmail = registrationvalidationdao.checkForRegistration(email);
 
-		userdao.insertion(user);
+		if (validEmail) {
+			User user = new User(email, password, role);
+
+			userdao.insertion(user);
+
+		} else {
+			response.sendRedirect("Register.jsp?message= User already registered..!");
+		}
 
 	}
 
