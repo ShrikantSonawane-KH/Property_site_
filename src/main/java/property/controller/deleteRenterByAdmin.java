dd19fa7c52939c2e6759e54ad5ed2859bb929d4f
Shrikant Sonawane_KH <83824454+ShrikantSonawane-KH@users.noreply.github.com>
2021-08-31 01:53:26 +0530

    Create deleteRenterByAdmin.java

---
+++ src/main/java/property/controller/deleteRenterByAdmin.java
@@ -0,0 +1,37 @@
+package property.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import property.dao.RenterDao;
+import property.dao.RenterDaoImpl;
+import property.pojo.Renter;
+
+public class deleteRenterByAdmin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public deleteRenterByAdmin() {
+		super();
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		RenterDao renterdao = new RenterDaoImpl();
+
+		int id = Integer.parseInt(request.getParameter("id"));
+
+		Renter renter = renterdao.deletion(id);
+
+		String propertyName = renter.getProperty().getName();
+
+		response.sendRedirect(
+				"editRenter.jsp?message= Successfully removed" + " " + propertyName + " " + "property...!");
+
+	}
+
+}
