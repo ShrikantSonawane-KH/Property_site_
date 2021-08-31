f59dc994fea629c586f526316468f7ff63827357
Shrikant Sonawane_KH <83824454+ShrikantSonawane-KH@users.noreply.github.com>
2021-09-01 01:27:32 +0530

    Update EmailConfirmation.java

--- src/main/java/property/login/confirmation/EmailConfirmation.java
+++ src/main/java/property/login/confirmation/EmailConfirmation.java
@@ -15,168 +15,162 @@ import javax.mail.internet.MimeMultipart;
 
 public class EmailConfirmation {
 	
-	public void sendEmail(String name,String email,String password) {
+	public void sendEmailAA(String name,String email,String password) {
 		
 		System.out.println("preparing to send message ...");
-		String message = "Hello Dear, Welcome to Residena..! Successfully completio";
+		String message = "Hello "+name+", Welcome to Residena...!\n You Have Registered Successfully.\n"
+				+ "Username : "+email+" Password : "+password+" \n Please save your credential for further login.";
 		String subject = "CodersArea : Confirmation";
-		String to = "adityamorajkar257@gmail.com";
+		String to = email;
 		String from = "residena12@gmail.com";
-
+		
 //		sendEmail(message,subject,to,from);
-		sendAttach(message, subject, to, from);
+		sendAttach(message,subject,to,from);
 	}
 
-	// this is responsible to send the message with attachment
-	private static void sendAttach(String message, String subject, String to, String from) {
+	//this is responsible to send the message with attachment
+			private static void sendAttach(String message, String subject, String to, String from) {
 
-		// Variable for gmail
-		String host = "smtp.gmail.com";
+				//Variable for gmail
+				String host="smtp.gmail.com";
+				
+				//get the system properties
+				Properties properties = System.getProperties();
+				System.out.println("PROPERTIES "+properties);
+				
+				//setting important information to properties object
+				
+				//host set
+				properties.put("mail.smtp.host", host);
+				properties.put("mail.smtp.port","465");
+				properties.put("mail.smtp.ssl.enable","true");
+				properties.put("mail.smtp.auth","true");
+				
+				//Step 1: to get the session object..
+				Session session=Session.getInstance(properties, new Authenticator() {
+					@Override
+					protected PasswordAuthentication getPasswordAuthentication() {				
+						return new PasswordAuthentication("Residena12@gmail.com", "Residena@123");
+					}
+				});
+				
+				session.setDebug(true);
+				
+				//Step 2 : compose the message [text,multi media]
+				MimeMessage m = new MimeMessage(session);
+				
+				try {
+				
+				//from email
+				m.setFrom(from);
+				
+				//adding recipient to message
+				m.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+				
+				//adding subject to message
+				m.setSubject(subject);
+		
+				//attachement..
+				
+				//file path
+				String path="C:\\Users\\Sharimila\\Desktop\\ResidenaLogo.jpg";
+				
+				MimeMultipart mimeMultipart = new MimeMultipart();
+				//text
+				//file
+				
+				MimeBodyPart textMime = new MimeBodyPart();
+				
+				MimeBodyPart fileMime = new MimeBodyPart();
+				
+				try {
+					
+					textMime.setText(message);
+					
+					File file=new File(path);
+					fileMime.attachFile(file);
 
-		// get the system properties
-		Properties properties = System.getProperties();
-		System.out.println("PROPERTIES " + properties);
+					mimeMultipart.addBodyPart(textMime);
+					mimeMultipart.addBodyPart(fileMime);
+						
+				} catch (Exception e) {
+					
+					e.printStackTrace();
+				}
+				
+				m.setContent(mimeMultipart);
+				
+				//send 
+				
+				//Step 3 : send the message using Transport class
+				Transport.send(m);
 
-		// setting important information to properties object
-
-		// host set
-		properties.put("mail.smtp.host", host);
-		properties.put("mail.smtp.port", "465");
-		properties.put("mail.smtp.ssl.enable", "true");
-		properties.put("mail.smtp.auth", "true");
-
-		// Step 1: to get the session object..
-		Session session = Session.getInstance(properties, new Authenticator() {
-			@Override
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication("Residena12@gmail.com", "Residena@123");
+				}catch (Exception e) {
+					e.printStackTrace();
+				}
+				
+				System.out.println("confirmation sent...!");
+					
 			}
 
-		});
-
-		session.setDebug(true);
-
-		// Step 2 : compose the message [text,multi media]
-		MimeMessage m = new MimeMessage(session);
-
-		try {
-
-			// from email
-			m.setFrom(from);
-
-			// adding recipient to message
-			m.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-			// adding subject to message
-			m.setSubject(subject);
-
-			// file path
-			String path = "C:\\Users\\Sharimila\\Desktop\\iphone.jpg";
-
-			MimeMultipart mimeMultipart = new MimeMultipart();
-			// text
-			// file
-
-			MimeBodyPart textMime = new MimeBodyPart();
-
-			MimeBodyPart fileMime = new MimeBodyPart();
-
-			try {
-
-				textMime.setText(message);
-
-				File file = new File(path);
-				fileMime.attachFile(file);
-
-				mimeMultipart.addBodyPart(textMime);
-				mimeMultipart.addBodyPart(fileMime);
-
-			} catch (Exception e) {
-
-				e.printStackTrace();
+			//this is responsible to send email..
+			private static void sendEmail(String message, String subject, String to, String from) {
+				
+				//Variable for gmail
+				String host="smtp.gmail.com";
+				
+				//get the system properties
+				Properties properties = System.getProperties();
+				System.out.println("PROPERTIES "+properties);
+				
+				//setting important information to properties object
+				
+				//host set
+				properties.put("mail.smtp.host", host);
+				properties.put("mail.smtp.port","465");
+				properties.put("mail.smtp.ssl.enable","true");
+				properties.put("mail.smtp.auth","true");
+				
+				//Step 1: to get the session object..
+				Session session=Session.getInstance(properties, new Authenticator() {
+					@Override
+					protected PasswordAuthentication getPasswordAuthentication() {				
+						return new PasswordAuthentication("Residena12@gmail.com", "Residena@123");
+					}
+				
+				});
+				
+				session.setDebug(true);
+				
+				//Step 2 : compose the message [text,multi media]
+				MimeMessage m = new MimeMessage(session);
+				
+				try {
+				
+				//from email
+				m.setFrom(from);
+				
+				//adding recipient to message
+				m.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+				
+				//adding subject to message
+				m.setSubject(subject);
+			
+				
+				//adding text to message
+				m.setText(message);
+				
+				//send 
+				
+				//Step 3 : send the message using Transport class
+				Transport.send(m);
+				
+				System.out.println("confirmation sent...!");
+				
+				}catch (Exception e) {
+					e.printStackTrace();
+				}
+					
 			}
-
-			m.setContent(mimeMultipart);
-
-			// send
-
-			// Step 3 : send the message using Transport class
-			Transport.send(m);
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		System.out.println("Sent success...................");
-
-	}
-
-	// this is responsible to send email..
-	private static void sendEmail(String message, String subject, String to, String from) {
-
-		// Variable for gmail
-		String host = "smtp.gmail.com";
-
-		// get the system properties
-		Properties properties = System.getProperties();
-		System.out.println("PROPERTIES " + properties);
-
-		// setting important information to properties object
-
-		// host set
-		properties.put("mail.smtp.host", host);
-		properties.put("mail.smtp.port", "465");
-		properties.put("mail.smtp.ssl.enable", "true");
-		properties.put("mail.smtp.auth", "true");
-
-		// Step 1: to get the session object..
-		Session session = Session.getInstance(properties, new Authenticator() {
-			@Override
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication("techsoftindia2018@gmail.com", "Durgesh@123");
-			}
-
-		});
-
-		session.setDebug(true);
-
-		// Step 2 : compose the message [text,multi media]
-		MimeMessage m = new MimeMessage(session);
-
-		try {
-
-			// from email
-			m.setFrom(from);
-
-			// adding recipient to message
-			m.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-			// adding subject to message
-			m.setSubject(subject);
-
-			// adding text to message
-			m.setText(message);
-
-			// send
-
-			// Step 3 : send the message using Transport class
-			Transport.send(m);
-
-			System.out.println("Sent success...................");
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
