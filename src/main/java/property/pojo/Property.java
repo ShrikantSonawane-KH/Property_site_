925a2b743fe310c43094458c968ef6e799c3f647
Shrikant Sonawane_KH <83824454+ShrikantSonawane-KH@users.noreply.github.com>
2021-09-04 00:34:38 +0530

    Update Property.java

--- src/main/java/property/pojo/Property.java
+++ src/main/java/property/pojo/Property.java
@@ -1,14 +1,7 @@
 package property.pojo;
 
 import javax.persistence.Embeddable;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
-import property.pojo.Pro_Description;
-import property.pojo.Pro_address;
-import property.pojo.Pro_image;
 
 @Embeddable
 public class Property {
